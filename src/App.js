0562133ada4d68a5ee7a2fca286dbe7e77427683
soykot2910 <ashrafulislam2910@gmail.com>
2021-09-15 17:30:13 +0600

    install dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <Route />
+    </Router>
   );
 };
 
