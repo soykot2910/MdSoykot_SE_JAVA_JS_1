322f0b54f0a478ac5d77209611b9f184a80cd130
soykot2910 <ashrafulislam2910@gmail.com>
2021-09-16 20:10:11 +0600

    about page design change

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,16 +1,206 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
 
 const About = () => {
   return (
     <div className="py-5">
-      <Container>
-        <Row>
-          <Col md={6}>
-            <h1>About Sweet iTech</h1>
-          </Col>
-        </Row>
-      </Container>
+      <div className="container">
+        <h1 className="text-center pt-2 pb-5">About Sweet iTech</h1>
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                The Leading Online Computer Shop in Bangladesh
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                If you are looking for the best computer shop in Bangladesh you
+                have to consider Ryans Computers, as it is a pioneer computer
+                shop and e-commerce platform selling computer and IT products
+                all over Bangladesh through its branches and website. It
+                provides a fast, secure, and convenient online shopping
+                experience with a broad product offering in categories ranging
+                from desktop PC, laptop to office equipment, camera, and smart
+                devices. Ryans is always endeavoring to offer its customers the
+                best possible facility – including multiple payment options, EMI
+                Facility, best price, cash on delivery, delivery in 64
+                districts, free home delivery inside Dhaka and Chattogram city,
+                24/7 online support, and extensive customer service and warranty
+                commitments.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Largest Chain of Computer Store in Bangladesh
+              </button>
+            </h2>
+            <div
+              id="collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingTwo"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                Besides online facility Ryans offers the largest chain of
+                computer stores in Bangladesh with its branches available in
+                Dhaka, Chattogram, Rajshahi, Khulna, Rangpur, and Barishal
+                division. We have stores at the most prominent IT market in
+                Dhaka like BCS Computer City (IDB Bhaban), Multiplan City Centre
+                & Eastern Plus Shopping Complex. Moreover, two large outlets are
+                there at the Banani and Uttara locations in Dhaka. The largest
+                store of Ryans is in Agrabad, Chattogram which is the largest
+                computer store in Bangladesh. In Chattogram Ryans has another
+                store at Younesco City Centre, GEC Circle. Ryans Khulna branch
+                is the largest computer store in the Khulna division. Branches
+                in Rangpur, Rajshahi, Bogura, Barishal, and Mymensingh districts
+                are also providing the same quality product and service just
+                like the branches from Dhaka
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                Best After-Sales & Warranty Support
+              </button>
+            </h2>
+            <div
+              id="collapseThree"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingThree"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                Our aim is to ensure the best quality after-sales service and
+                warranty support for our customers. We are providing warranty
+                and after-sales service from each of our branches. Anyone can
+                get after-sales service from any of our branches, no matter from
+                which branch he or she purchased the product.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingFour">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseFour"
+                aria-expanded="false"
+                aria-controls="collapseFour"
+              >
+                Largest IT product collection in Bangladesh
+              </button>
+            </h2>
+            <div
+              id="collapseFour"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingFour"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                We are constantly expanding our product range as well as
+                improving our service quality. We offer the best selection of
+                quality products that you can buy online or purchase from our
+                outlets. Stay connected and stay tuned for new exciting
+                products. Remember, we have the largest collection of IT
+                products in Bangladesh. Lastly, it is our highest priority here
+                at ryanscomputers.com to create the best online shopping
+                experience for every customer in Bangladesh. So start your
+                online shopping and remember if you have questions we are just a
+                phone call or email away.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingFive">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseFive"
+                aria-expanded="false"
+                aria-controls="collapseFive"
+              >
+                Best Laptop Shop in Bangladesh
+              </button>
+            </h2>
+            <div
+              id="collapseFive"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingFour"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                High-quality laptops from renowned brands like Apple, Microsoft,
+                HP, Dell, Asus, Acer, Lenovo, Gigabyte, Huawei, Chuwi, i-Life,
+                Nextgo, and more are available at the best price in Ryans. Some
+                laptops come with Intel processors like Pentium, Core i3, Core
+                i5, Core i7, and Core i9. On the other hand, some laptops are
+                AMD processor-based and include processors like Ryzen 3, Ryzen
+                5, Ryzen 7, Ryzen 9, etc.
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingSix">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseSix"
+                aria-expanded="false"
+                aria-controls="collapseSix"
+              >
+                Best Computer Shop in Bangladesh
+              </button>
+            </h2>
+            <div
+              id="collapseSix"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingFour"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body">
+                We are selling Brand PC, mini PC, all in one PC. Also you can
+                build your desired personal computer with our expert
+                consultancy. Ryans Computers is the oldest computers accessories
+                seller in Bangladesh. You can buy from us Proccessor,
+                motherboard, desktop ram, desktop cooler, casing, SSD, hard
+                disk, graphics card etc. Computer parts
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
